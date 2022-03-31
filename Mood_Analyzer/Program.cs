8db9e07cfa01798aaf4c0187d8a8458c3c8d32bf
Tushar Anand <tusharranand@gmail.com>
2022-03-31 21:10:45 +0530

    Introduction

---
+++ Mood_Analyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mood_Analyzer
+{
+    class Mood_Analyzer_Program
+    {
+        public Mood_Analyzer_Program()
+        {
+
+        }
+        
+    }
+}
---
+++ Mood_Analyzer_Testing/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Mood_Analyzer;
+using NUnit.Framework;
+
+namespace Mood_Analyzer_Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
