ccc5c766df39d98649b64de148735b721a7cc88d
Tushar Anand <tusharranand@gmail.com>
2022-04-01 12:18:59 +0530

    [Tushar Anand] Added: Analyze mood of a given string using parameterised method

--- Mood_Analyzer/Program.cs
+++ Mood_Analyzer/Program.cs
@@ -2,12 +2,21 @@
 
 namespace Mood_Analyzer
 {
-    class Mood_Analyzer_Program
+    public class Mood_Analyzer_Program
     {
         public Mood_Analyzer_Program()
         {
 
         }
-        
+        public string MoodAnalyzer(string Message)
+        {
+            if (Message.ToUpper().Contains("SAD"))
+                return "SAD";
+            else return "HAPPY";
+        }
+        public static void Main(string[] args)
+        {
+
+        }
     }
 }
--- Mood_Analyzer_Testing/UnitTest1.cs
+++ Mood_Analyzer_Testing/UnitTest1.cs
@@ -5,15 +5,29 @@ namespace Mood_Analyzer_Testing
 {
     public class Tests
     {
+        Mood_Analyzer_Program analyze;
         [SetUp]
         public void Setup()
         {
+           analyze = new Mood_Analyzer_Program();
         }
-
+        /// <summary>
+        /// TC 1.1: Given “I am in Sad Mood” message Should Return SAD
+        /// </summary>
         [Test]
-        public void Test1()
+        public void When_Given_Sad_Message_Should_Return_SAD()
         {
-            Assert.Pass();
+            string Returned_Message = analyze.MoodAnalyzer("I am in sad mood");
+            Assert.AreEqual("SAD", Returned_Message);
+        }
+        /// <summary>
+        /// TC 1.2: Given “I am in Any Mood” message Should Return HAPPY
+        /// </summary>
+        [Test]
+        public void When_Given_Any_Message_Should_Return_HAPPY()
+        {
+            string Returned_Message = analyze.MoodAnalyzer("I am in any mood");
+            Assert.AreEqual("HAPPY", Returned_Message);
         }
     }
 }
